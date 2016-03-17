3bab7975a99d361d8161a9db99452877dc760464
TJ Luoma <luomat@gmail.com>
2016-03-17 13:27:53 -0400

    Initial Commit

---
+++ uninstall.sh
@@ -0,0 +1,50 @@
+#!/bin/zsh -f
+
+NAME="$0:t:r"
+
+PLIST='/Library/LaunchDaemons/com.tjluoma.setrestartpowerfailure.on.plist'
+
+if [[ ! -e "$PLIST" ]]
+then
+	echo "$NAME: $PLIST does not exist."
+	exit 0
+fi 
+
+function die
+{
+	echo "$NAME: $@"
+	exit 1
+}
+
+sudo -v -p "
+
+	Your administrator password is required to continue. 
+	This is the password that you use to log in to your '%u' account on this computer.
+	Password: "
+
+####################################################################################
+
+LABEL=`defaults read "$PLIST" Label`
+
+sudo launchctl stop "$LABEL" \
+|| die "launchctl stop: failed"
+
+####################################################################################
+
+sudo launchctl unload "$PLIST" \
+|| die "launchctl unload failed"
+
+####################################################################################
+
+sudo rm -f "$PLIST" \
+|| die "Failed to remove $PLIST"
+
+echo "$NAME: rm (remove) $PLIST was successful"
+
+####################################################################################
+
+echo "$NAME: Exiting after all steps were successful."
+
+####################################################################################
+
+exit 0
